bbb73813eb6cb0101fe7dcaa1a09766363b97442
rathi <ranveer@syncrasytech.com>
2022-12-22 14:31:25 +0530

    object open issues

--- Resources/public/js/pimcore/startup.js
+++ Resources/public/js/pimcore/startup.js
@@ -13,81 +13,90 @@ pimcore.plugin.SyncrasyPimcoreSalesforceBundle = Class.create(pimcore.plugin.adm
         // alert("SyncrasyPimcoreSalesforceBundle ready!");
     },
     postOpenObject: function (object, type) {
-        console.log('-------------');
         if (typeof object.edit != 'undefined' && object.data.general.o_className == 'SelesForceSetup') {
             var pimClassChange = object.edit.dataFields.pimcoreclass.component;
             var objectData = object.data.data.fieldmapping;
-            console.log('-------------',object.data.data);
             var pimClassFields;
+            var sfObjectFields;
             var sfObjectChange = object.edit.dataFields.salesforceobject.component;
-            SyncrasyPimcoreSalesforceBundlePlugin.getSfClassAjax(function(store){
+            var tabPanel = object.edit.layout.items.items[0];
+
+            SyncrasyPimcoreSalesforceBundlePlugin.getSfClassAjax(function (store) {
                 sfObjectChange.setStore(store);
                 sfObjectChange.setValue(object.data.data.salesforceobject);
-                console.log('------sfObjectChange.getValue()-------',sfObjectChange.getValue());
-                if(sfObjectChange.getValue()!='') {
-                SyncrasyPimcoreSalesforceBundlePlugin.getSfFieldAjax(sfObjectChange.getValue(), function (store) {
-                    sfObjectFields = store;
-                });
-            }
-                
+                if (sfObjectChange.getValue() != '') {
+                    SyncrasyPimcoreSalesforceBundlePlugin.getSfFieldAjax(sfObjectChange.getValue(), function (store) {
+                        sfObjectFields = store;
+                        SyncrasyPimcoreSalesforceBundlePlugin.setFieldMapping(tabPanel.items.items[1], pimClassFields, sfObjectFields, objectData);
+                    });
+                }
             });
-            SyncrasyPimcoreSalesforceBundlePlugin.getPimClassAjax(function(store){
+            SyncrasyPimcoreSalesforceBundlePlugin.getPimClassAjax(function (store) {
                 pimClassChange.setStore(store);
                 pimClassChange.setValue(object.data.data.pimcoreclass);
-                if(pimClassChange.getValue() != '') {
-                SyncrasyPimcoreSalesforceBundlePlugin.getPimFieldAjax(pimClassChange.getValue(), function (store) {
-                    pimClassFields = store;
-                });
-            }
+                if (pimClassChange.getValue() != '') {
+                    SyncrasyPimcoreSalesforceBundlePlugin.getPimFieldAjax(pimClassChange.getValue(), function (store) {
+                        pimClassFields = store;
+                        SyncrasyPimcoreSalesforceBundlePlugin.setFieldMapping(tabPanel.items.items[1], pimClassFields, sfObjectFields, objectData);
+                    });
+                }
             });
+
+
             pimClassChange.addListener('select', function (combo, record, eOpts) {
-                if(record.data.value != '') {
+                if (record.data.value != '') {
                     SyncrasyPimcoreSalesforceBundlePlugin.getPimFieldAjax(record.data.value, function (store) {
                         pimClassFields = store;
                     });
                 }
             });
-            
-            
-
-
-            
-            
-            var sfObjectFields;
             sfObjectChange.addListener('select', function (combo, record, eOpts) {
-                if(record.data.value != '') {
+                if (record.data.value != '') {
                     SyncrasyPimcoreSalesforceBundlePlugin.getSfFieldAjax(record.data.value, function (store) {
                         sfObjectFields = store;
                     });
                 }
             });
-            
-           
+
 
 
             var tabPanel = object.edit.layout.items.items[0];
+
             tabPanel.addListener('tabchange', function (tabPanel, newTab, oldTab, eOpts) {
                 if (newTab.title == 'Fields Mapping') {
-                    newTab.items.items[0].addListener('add', function (panelAdd, addBlock) {
-                        if (addBlock.items.items.length > 1) {
-                            addBlock.items.items[1].items.items[0].setStore(pimClassFields);
-                            addBlock.items.items[1].items.items[1].setStore(sfObjectFields);
-                        }
-                    });
-                    var i = 0;
-                    
-                    newTab.items.items[0].items.items.forEach(function (item) {
-                        item.items.items[1].items.items[0].setStore(pimClassFields);
-                        if(objectData[i] !== undefined)
-                        item.items.items[1].items.items[0].setValue(objectData[i].data.pimcoreclassfield);
-
-                        item.items.items[1].items.items[1].setStore(sfObjectFields);
-                        if(objectData[i] !== undefined)
-                        item.items.items[1].items.items[1].setValue(objectData[i].data.salesforceobjectfield);
-                        i++;
-                    });
+                    SyncrasyPimcoreSalesforceBundlePlugin.setFieldMapping(newTab, pimClassFields, sfObjectFields, objectData);
                 }
             });
+        }
+
+    },
+
+    setFieldMapping: function (newTab, pimClassFields, sfObjectFields, objectData) {
+
+
+        if (newTab.items.items.length > 0) {
+            newTab.items.items[0].addListener('add', function (panelAdd, addBlock) {
+                if (addBlock.items.items.length > 1) {
+                    addBlock.items.items[1].items.items[0].setStore(pimClassFields);
+                    addBlock.items.items[1].items.items[1].setStore(sfObjectFields);
+                }
+            });
+            var i = 0;
+            if (newTab.items.items[0].items.items.length > 0) {
+                newTab.items.items[0].items.items.forEach(function (item) {
+                    if (pimClassFields !== undefined) {
+                        item.items.items[1].items.items[0].setStore(pimClassFields);
+                        if (objectData[i] !== undefined)
+                            item.items.items[1].items.items[0].setValue(objectData[i].data.pimcoreclassfield);
+                    }
+                    if (sfObjectFields) {
+                        item.items.items[1].items.items[1].setStore(sfObjectFields);
+                        if (objectData[i] !== undefined)
+                            item.items.items[1].items.items[1].setValue(objectData[i].data.salesforceobjectfield);
+                    }
+                    i++;
+                });
+            }
         }
 
     },
@@ -101,7 +110,6 @@ pimcore.plugin.SyncrasyPimcoreSalesforceBundle = Class.create(pimcore.plugin.adm
                 let data = Ext.JSON.decode(response.responseText);
                 if (data.success) {
                     options = data.data;
-                    console.log('--------options-------',options);
                     pimClassFields = Ext.create('Ext.data.Store', {
                         fields: ['value', 'key'],
                         data: options
@@ -181,7 +189,7 @@ pimcore.plugin.SyncrasyPimcoreSalesforceBundle = Class.create(pimcore.plugin.adm
         });
 
     }
-    
+
 });
 
 var SyncrasyPimcoreSalesforceBundlePlugin = new pimcore.plugin.SyncrasyPimcoreSalesforceBundle();
